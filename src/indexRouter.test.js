9a67bfacbb9d50ff375f50b82c4f833671310315
Joshua Turner <joshua.j.turner@hotmail.com>
2022-10-12 23:37:45 +0100

    Add missing tests

--- src/indexRouter.test.js
+++ src/indexRouter.test.js
@@ -1,8 +1,15 @@
-const {
-  getConsoleLogMocks,
-  restoreMockConsoleLogs,
-} = require("./utils/tests/mock");
-const { db, req } = require("./utils/tests/server");
+let getConsoleLogMocks;
+let restoreMockConsoleLogs;
+let db;
+let req;
+jest.isolateModules(() => {
+  const mock = require("./utils/tests/mock");
+  getConsoleLogMocks = mock.getConsoleLogMocks;
+  restoreMockConsoleLogs = mock.restoreMockConsoleLogs;
+  const server = require("./utils/tests/server");
+  db = server.db;
+  req = server.req;
+});
 
 let consoleLogMocks;
 beforeAll(async () => {
@@ -26,5 +33,30 @@ describe("GET /", () => {
       status: "Connect Four JS Running!",
     });
     expect(res.statusCode).toBe(200);
+  });
+});
+
+describe("GET /swagger", () => {
+  const originalEnv = process.env;
+  beforeEach(() => {
+    process.env = {
+      ...originalEnv,
+    };
+  });
+
+  afterEach(async () => {
+    process.env = originalEnv;
+  });
+
+  it('Should connect if the NODE_ENV is not "production"', async () => {
+    const res = await req.get("/swagger");
+    expect(res.statusCode).toBe(200);
+  });
+
+  it('Should not connect if the NODE_ENV is "production"', async () => {
+    process.env.NODE_ENV = "production";
+    const { req: req2 } = require("./utils/tests/server");
+    const res = await req2.get("/swagger");
+    expect(res.statusCode).toBe(404);
   });
 });
